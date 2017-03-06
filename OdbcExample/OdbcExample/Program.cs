828b0bacd4112450dbf4ae5f5e7dc6d917c2eed2
PackJunYoung <jyjyjy89@naver.com>
2017-03-06 20:49:44 +0900

    commit1

---
+++ OdbcExample/OdbcExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+
+namespace OdbcExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string myConnString = "DRIVER=MysqlODBC;SERVER=MyServer;Trusted_connection=yes;DATABASE=shopdb;";
+                OdbcConnection myConnection = new OdbcConnection(myConnString);
+                myConnection.Open();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            SqlConnection connection = new SqlConnection("");
+
+            Console.ReadLine();
+        }
+    }
+}
